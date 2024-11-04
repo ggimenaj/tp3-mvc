729aed08d344eef2ba85d80c65fa4c305e1cc5c1
ggimenaj <gimenabigailjimenez7@gmail.com>
2024-11-04 04:53:31 -0300

    trabajo final

---
+++ src/config/dbConfig.mjs
@@ -0,0 +1,14 @@
+
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Grupo-14:grupo14@cursadanodejs.ls9ii.mongodb.net/Node-js"
+    );
+    console.log("conexion exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB", error);
+    process.exit(1);
+  }
+}
